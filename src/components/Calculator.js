e05ab6ae08470b014300bb5b3c181e035deaeb36
psycd8 <psycd8@exmail.nottingham.ac.uk>
2023-01-10 12:09:42 +0000

    feat(ui): created and styled basic ui
    Created basic user interface based on figma design.
    This UI has no actual functionality, but buttons will react to
    the cursor (i.e. on hover and on click).

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,7 +3,45 @@ import React from "react"
 export default function Calculator() {
     return (
         <div className="calculator">
-            <h1>Calculator</h1>
+            <input 
+                type="text" 
+                className="calculator--screen" 
+            />
+            <div className="calculator--input">
+                <div className="calculator--numbers">
+                    <span className="number-row">
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                    </span>
+                    <span className="number-row">
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                    </span>
+                    <span className="number-row">
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                    </span>
+                    <span className="number-row">
+                            <button>.</button>
+                            <button>&#40;</button>
+                            <button>&#41;</button>
+                    </span>
+                </div>
+                <div className="calculator--functions">
+                    <button>+</button>
+                    <button>-</button>
+                    <button>*</button>
+                    <button>/</button>
+                    <button>=</button>
+                </div>
+                <div className="calculator--controls">
+                    <button>BACK</button>
+                    <button>CLEAR</button>
+                </div>
+            </div>
         </div>
     )
 }
