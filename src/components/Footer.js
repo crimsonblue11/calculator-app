db2d2b88a21d1a8ac95048629111c08eb1aef3d2
psycd8 <psycd8@exmail.nottingham.ac.uk>
2023-01-10 11:36:21 +0000

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Created and maintained by Conor Dempsey, 2023</p>
+        </footer>
+    )
+}
