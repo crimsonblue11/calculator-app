adb3f726211287c8e76d2ed871e823998043b5a4
psycd8 <psycd8@exmail.nottingham.ac.uk>
2023-01-10 12:32:47 +0000

    feat: add basic functionality to buttons
    Add basic functionality to number and mathematical functions.
    These update the state of the calculator's screen.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,40 +1,51 @@
 import React from "react"
 
 export default function Calculator() {
+    const [calcState, setCalcState] = React.useState("")
+
+    function addToState(event) {
+        const {value} = event.target
+
+        setCalcState(prevState => prevState + value)
+    }
+
     return (
         <div className="calculator">
             <input 
                 type="text" 
-                className="calculator--screen" 
+                className="calculator--screen"
+                value={calcState}
+                readOnly={true}
+
             />
             <div className="calculator--input">
                 <div className="calculator--numbers">
                     <span className="number-row">
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
+                        <button value={"1"} onClick={addToState}>1</button>
+                        <button value={"2"} onClick={addToState}>2</button>
+                        <button value={"3"} onClick={addToState}>3</button>
                     </span>
                     <span className="number-row">
-                        <button>4</button>
-                        <button>5</button>
-                        <button>6</button>
+                        <button value={"4"} onClick={addToState}>4</button>
+                        <button value={"5"} onClick={addToState}>5</button>
+                        <button value={"6"} onClick={addToState}>6</button>
                     </span>
                     <span className="number-row">
-                        <button>7</button>
-                        <button>8</button>
-                        <button>9</button>
+                        <button value={"7"} onClick={addToState}>7</button>
+                        <button value={"8"} onClick={addToState}>8</button>
+                        <button value={"9"} onClick={addToState}>9</button>
                     </span>
                     <span className="number-row">
-                            <button>.</button>
-                            <button>&#40;</button>
-                            <button>&#41;</button>
+                            <button value={"0"} onClick={addToState}>0</button>
+                            <button value={"("} onClick={addToState}>&#40;</button>
+                            <button value={")"} onClick={addToState}>&#41;</button>
                     </span>
                 </div>
                 <div className="calculator--functions">
-                    <button>+</button>
-                    <button>-</button>
-                    <button>*</button>
-                    <button>/</button>
+                    <button value={"+"} onClick={addToState}>+</button>
+                    <button value={"-"} onClick={addToState}>-</button>
+                    <button value={"*"} onClick={addToState}>*</button>
+                    <button value={"/"} onClick={addToState}>/</button>
                     <button>=</button>
                 </div>
                 <div className="calculator--controls">
