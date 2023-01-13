486d9f71e5e89c2a5174de130c83b9f40a2a862c
psycd8 <psycd8@exmail.nottingham.ac.uk>
2023-01-13 11:58:22 +0000

    feat: add link icons to footer
    Add Github and Linkedin link icons to footer. Both links work
    as intended.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from "react"
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai"
 
 export default function Footer() {
     return (
         <footer>
             <p>Created and maintained by Conor Dempsey, 2023</p>
+            <span className="footer--icons">
+                <a href="https://github.com/crimsonblue11"><AiFillGithub /></a>
+                <a href="https://www.linkedin.com/in/conor-dempsey-a66269237"><AiFillLinkedin /></a>
+            </span>
         </footer>
     )
 }
