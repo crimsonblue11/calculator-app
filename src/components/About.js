06cad769638d2c0f60c21c50188b9a04f74843eb
psycd8 <psycd8@exmail.nottingham.ac.uk>
2023-01-13 12:51:25 +0000

    feat: add about page
    Add about page. Technically this is a component rather than
    an actual page, but it functions the same.

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <article>
+            <h1>About</h1>
+            <p>Hi! This is a student project I built using React.
+            I've built calculators before, but I'm most proud of this one.</p>
+            <p>I learned React almost entirely 
+            from <a href="https://www.youtube.com/watch?v=bMknfKXIFA8">this</a> tutorial, 
+            so shout out to Bob Ziroll.
+            If you've never check out freecodecamp I highly highly recommend them.</p>
+            <p>Anyway, if you want any of my links the important ones are in the Footer.</p>
+            <p>Cheers!</p>
+            <p>- Conor Dempsey, author and maintiner</p>
+        </article>
+    )
+}
